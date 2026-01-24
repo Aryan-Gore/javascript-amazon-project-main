caab42de2d9c01e43972b71d4d6a164d1e46528c
Aryan Gore <aryangore130@gmail.com>
2026-01-24 22:04:55 +0530

    final cart update with localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,17 @@
-export let cart = [
+export let cart =  JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+   cart = [
     {
         productId :'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity : 1
     }
 ];
+} 
 
+
+function saveToStorage(){
+    localStorage.setItem('cart' ,JSON.stringify(cart))
+}
 export function addToCart(productId){
          // check for if button alreaady exist
         let matchingItem;
@@ -23,6 +30,7 @@ export function addToCart(productId){
             quantity : 1
         })
         }
+        saveToStorage();
     }
      // total No. of items present in cart
    export function UpdateCartQuantity(){
@@ -47,4 +55,5 @@ export  function removefromcart(productId){
         })
 
         cart = newcart;
+        saveToStorage();
     }
